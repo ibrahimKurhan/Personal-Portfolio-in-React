7cc690ba16acd23feee01dc7ff7355c0fba008e2
Ibrahim Kurhan <ikurhan@outlook.com>
2020-05-15 17:56:31 +0300

    typical added

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import styles from "./Home.module.scss";
-
+import Typical from "react-typical";
 export default class Home extends Component {
   render() {
     return (
@@ -8,7 +8,15 @@ export default class Home extends Component {
         <a name="home" />
         <div className={styles.top} id={styles.home}>
           <div className={styles.header}>
-            <div className={styles.head}>Hello I'm Ibrahim Kurhan</div>
+            <div className={styles.head}>
+              Hello I'm{" "}
+              <Typical
+                loop={Infinity}
+                wrapper={"b"}
+                steps={["Ibrahim Kurhan", 1000]}
+              />
+            </div>
+            <h2 id="element"></h2>
             <div className={styles.junior}>
               Junior Software Developer based in London
             </div>
