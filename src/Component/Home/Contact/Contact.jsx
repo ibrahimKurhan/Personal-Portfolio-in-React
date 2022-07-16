66b3b55498ee8e87fef4f9c9cb9ec3226ba3d500
Ibrahim Kurhan <ikurhan@outlook.com>
2022-07-16 02:04:19 +0100

    remove space

--- src/Component/Home/Contact/Contact.jsx
+++ src/Component/Home/Contact/Contact.jsx
@@ -8,30 +8,28 @@ import linkedinn from "../../../Image/linkedin2.png";
 export default class Contac extends Component {
   render() {
     return (
-      <div className={styles.information}>
-        <div className={styles.get}>Get in touch</div>
-        <div className={styles.HR}>
+      <div className={ styles.information }>
+        <div className={ styles.get }>Get in touch</div>
+        <div className={ styles.HR }>
           <hr />
         </div>
-
-        <div className={styles.social}>
-          <div className={styles.link}>
+        <div className={ styles.social }>
+          <div className={ styles.link }>
             <a href="https://github.com/ibkibk" target="_blank">
-              <img src={github} alt="Link to GitHub" />
+              <img src={ github } alt="Link to GitHub" />
             </a>
           </div>
-          <div className={styles.link}>
-            <a href={ibkCV} target="_blank">
-              <img src={cv} alt="Link to CV" />
+          <div className={ styles.link }>
+            <a href={ ibkCV } target="_blank">
+              <img src={ cv } alt="Link to CV" />
             </a>
           </div>
-
-          <div className={styles.link}>
+          <div className={ styles.link }>
             <a
               href="https://www.linkedin.com/in/ibrahim-kurhan-6971a827/"
               target="_blank"
             >
-              <img src={linkedinn} alt="Link to LinkedIn" />
+              <img src={ linkedinn } alt="Link to LinkedIn" />
             </a>
           </div>
         </div>
