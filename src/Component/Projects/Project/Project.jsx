e1785de38ca841fd0791c73975c85f8cba727654
Ibrahim Kurhan <ikurhan@outlook.com>
2022-07-16 02:25:19 +0100

    remove cookibot

--- src/Component/Projects/Project/Project.jsx
+++ src/Component/Projects/Project/Project.jsx
@@ -5,33 +5,32 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default class Project extends Component {
   render() {
-    let { id, image, name, description, link1, link2 } = this.props.pro;
+    let { image, name, description, link1, link2 } = this.props.pro;
     return (
-      <div className={style.cardBox}>
-        <div className={style.card}>
-          <div className={style.front}>
-            <div className={style.subtitle}>
-              <img src={image} />
+      <div className={ style.cardBox }>
+        <div className={ style.card }>
+          <div className={ style.front }>
+            <div className={ style.subtitle }>
+              <img src={ image } alt="test mage" />
             </div>
           </div>
-          <div className={style.back}>
-            <div className={style.title}>{name}</div>
-            <div className={style.des}>{description}</div>
-            <div className={style.link}>
-              <a href={link1} target="_blank">
-                {" "}
+          <div className={ style.back }>
+            <div className={ style.title }>{ name }</div>
+            <div className={ style.des }>{ description }</div>
+            <div className={ style.link }>
+              <a href={ link1 } target="_blank" rel="noopener noreferrer">
                 <FontAwesomeIcon
-                  style={{ cursor: "pointer" }}
-                  src={link1}
-                  icon={faCode}
+                  style={ { cursor: "pointer" } }
+                  src={ link1 }
+                  icon={ faCode }
                   size="4x"
                 />
               </a>
-              <a href={link2} target="_blank">
+              <a href={ link2 } target="_blank" rel="noopener noreferrer">
                 <FontAwesomeIcon
-                  src={link2}
-                  style={{ cursor: "pointer", marginLeft: "50px" }}
-                  icon={faGlobe}
+                  src={ link2 }
+                  style={ { cursor: "pointer", marginLeft: "50px" } }
+                  icon={ faGlobe }
                   size="4x"
                 />
               </a>
